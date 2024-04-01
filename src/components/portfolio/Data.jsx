91540f11c10b9dd1b8d7cd3d7c99daae9e5cebce
Marvin Patangan <marvn.patangan@gmail.com>
2024-04-01 20:09:15 +0800

    Updated images

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,8 +1,8 @@
-import Work1 from '../../assets/work1.jpg'
-import Work2 from '../../assets/work2.jpg'
-import Work3 from '../../assets/work3.jpg'
-import Work4 from '../../assets/work4.jpg'
-import Work5 from '../../assets/work5.jpg'
+import Work1 from '../../assets/work1.png'
+import Work2 from '../../assets/work2.png'
+import Work3 from '../../assets/work3.png'
+import Work4 from '../../assets/work4.png'
+import Work5 from '../../assets/work5.png'
 
 export const projectsData = [
     {
